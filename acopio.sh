b30351f0317d84e69c7f966150cf8a6e475cbc99
Conchi Carcedo Carnero <conchicarcedo@correo.ugr.es>
2017-11-26 23:53:48 +0100

    correccion errores acopio

--- acopio.sh
+++ acopio.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Crear el grupo de recursos
-az group create --name CCMiga --location eastus
+az group create --name GroupMiga --location eastus
 
 # Crear la maquina virtual con el grupo de recursos anterior.
-ipAddress=$(az vm create -g CCMiga -n remoteAzureMiga --image UbuntuLTS --generate-ssh-keys | jq -r '.publicIpAddress')
+ipAddress=$(az vm create -g GroupMiga -n MVMiga --image UbuntuLTS --generate-ssh-keys | jq -r '.publicIpAddress')
 
 echo Se ha creado la siguiente maquina:
-echo -name : remoteAzureMiga
+echo -name : MVMiga
 echo -ip : $ipAddress
 echo -------------------------
 
 # Abrir puertos
-az network nsg create --resource-group CCMiga --location eastus --name remoteAzureMigaNSG
-az network nsg rule create --resource-group CCMiga --nsg-name remoteAzureMigaNSG --name remoteAzureMigaNSGRule80 --protocol tcp --priority 1000 --destination-port-range 80
-az network nsg rule create --resource-group CCMiga --nsg-name remoteAzureMigaNSG --name remoteAzureMigaNSGRule20 --protocol tcp --priority 999 --destination-port-range 20
-az network nsg rule create --resource-group CCMiga --nsg-name remoteAzureMigaNSG --name remoteAzureMigaNSGRule22 --protocol tcp --priority 998 --destination-port-range 22
+az network nsg create --resource-group GroupMiga --location eastus --name remoteAzureMigaNSG
+az network nsg rule create --resource-group GroupMiga --nsg-name remoteAzureMigaNSG --name remoteAzureMigaNSGRule80 --protocol tcp --priority 1000 --destination-port-range 80
+az network nsg rule create --resource-group GroupMiga --nsg-name remoteAzureMigaNSG --name remoteAzureMigaNSGRule20 --protocol tcp --priority 999 --destination-port-range 20
+az network nsg rule create --resource-group GroupMiga --nsg-name remoteAzureMigaNSG --name remoteAzureMigaNSGRule22 --protocol tcp --priority 998 --destination-port-range 22
 
 #provision
 echo A continuación utilizamos la provisión con chef-solo:
